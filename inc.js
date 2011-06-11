8fb55968055932cef28e7cee568479f1174cc27f
Linuxmotion <linuxmotion@thelinuxmotion.org>
2011-06-10 23:51:29 -0700

    Add test json file for addons nighly manifests

---
+++ inc.js
@@ -0,0 +1,18 @@
+[
+{
+"device"="Incredible",
+"base"="1.3.0",
+"version"=3,
+"date"="Some date",
+"url"="http://r2doesinc.bitsurge.net/OMFGB-inc1.3.0_nightyly-3.zip"
+},
+
+{
+"device"="Incredible",
+"base"="1.3.0",
+"version"=2,
+"date"="Some date",
+"url"="http://r2doesinc.bitsurge.net/OMFGB-inc1.3.0_nightyly-3.zip"
+}
+]
+
