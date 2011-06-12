f98b7698b1447cca2dce5b5454e66e107d0dd3d5
David <davidjr621@gmail.com>
2011-06-12 02:21:38 -0700

    Added Nightly #2, Was missing

--- inc.js
+++ inc.js
@@ -26,9 +26,15 @@
 {
 "device"="Incredible",
 "base"="1.3.0",
+"version"=2,
+"date"="Some date",
+"url"="http://goo.gl/Ve9n8"
+
+{
+"device"="Incredible",
+"base"="1.3.0",
 "version"=1,
 "date"="Some date",
 "url"="http://r2doesinc.bitsurge.net/nightlies/Inc/OMFGB_inc-1.3.0_nightly-1.zip"
 }
 ]
-
