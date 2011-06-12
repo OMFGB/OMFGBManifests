472f0c00115c27072c360c3a7c89e7418e0c2e27
David <davidjr621@gmail.com>
2011-06-12 02:29:50 -0700

    Added Nightly #5

---
+++ sholes.js
@@ -0,0 +1,10 @@
+[
+{
+"device"="Sholes",
+"base"="1.3.0",
+"version"=5,
+"date"="June 12, 2011",
+"url"="http://r2doesinc.bitsurge.net/nightlies/sholes/OMFGB_sholes-1.3.0_nightly-5.zip"
+"installable"="false"
+}
+]
