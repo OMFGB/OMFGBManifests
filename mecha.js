132de9607eac683eb37122075c20ed3ff8a35459
David <davidjr621@gmail.com>
2011-06-12 02:28:31 -0700

    Added Nightly #4

---
+++ mecha.js
@@ -0,0 +1,10 @@
+[
+{
+"device"="Incredible",
+"base"="1.3.0",
+"version"=4,
+"date"="June 11, 2011",
+"url"="http://r2doesinc.bitsurge.net/nightlies/mecha/OMFGB_mecha-1.3.0_nightly-4.zip"
+"installable"="false"
+}
+]
