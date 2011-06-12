d4fb49d82186996390cb77b681791c64d759e608
David <davidjr621@gmail.com>
2011-06-12 02:31:31 -0700

    Added Nightly #4

---
+++ supersonic.js
@@ -0,0 +1,10 @@
+[
+{
+"device"="Supersonic",
+"base"="1.3.0",
+"version"=4,
+"date"="Some date",
+"url"="http://r2doesinc.bitsurge.net/nightlies/supersonic/OMFGB_supersonic-1.3.0_nightly-4.zip"
+"installable"="false"
+}
+]
