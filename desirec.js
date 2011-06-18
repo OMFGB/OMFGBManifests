949f719926672c3e7522095df8c3b754937699e3
Linuxmotion <linuxmotion@thelinuxmotion.org>
2011-06-17 21:12:28 -0700

    Rename to desirec directory

--- desirec.js
+++ desirec.js
@@ -5,7 +5,7 @@
 "version"=8,
 "date"="June 12, 2011",
 "name"="OMFGB_sholes-1.3.0_nightly-8.zip",
-"url"="http://r2doesinc.bitsurge.net/nightlies/sholes/OMFGB_desirec-1.3.0_nightly-8.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/desirec/OMFGB_desirec-1.3.0_nightly-8.zip",
 "installable"="false"
 }
 ]
--- sholes.js
+++ sholes.js
@@ -4,7 +4,8 @@
 "base"="1.3.0",
 "version"=5,
 "date"="June 12, 2011",
-"url"="http://r2doesinc.bitsurge.net/nightlies/sholes/OMFGB_sholes-1.3.0_nightly-5.zip"
+"name"="OMFGB_sholes-1.3.0_nightly-5.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/sholes/OMFGB_sholes-1.3.0_nightly-5.zip",
 "installable"="false"
 }
 ]
