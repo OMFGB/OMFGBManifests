a649d71e166d380ce0a17e724f1ad800b341c784
Linuxmotion <linuxmotion@thelinuxmotion.org>
2011-06-17 20:58:42 -0700

    Add nightlies 6,7 and 8

--- inc.js
+++ inc.js
@@ -2,6 +2,34 @@
 {
 "device"="Incredible",
 "base"="1.3.0",
+"version"=8,
+"date"="Some date",
+"name"="OMFGB_inc-1.3.0_nightly-8.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/Inc/OMFGB_inc-1.3.0_nightly-8.zip",
+"installable"="false"
+},
+{
+"device"="Incredible",
+"base"="1.3.0",
+"version"=7,
+"date"="Some date",
+"name"="OMFGB_inc-1.3.0_nightly-7.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/Inc/OMFGB_inc-1.3.0_nightly-7.zip",
+"installable"="false"
+},
+{
+"device"="Incredible",
+"base"="1.3.0",
+"version"=6,
+"date"="Some date",
+"name"="OMFGB_inc-1.3.0_nightly-6.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/Inc/OMFGB_inc-1.3.0_nightly-6.zip",
+"installable"="false"
+},
+
+{
+"device"="Incredible",
+"base"="1.3.0",
 "version"=5,
 "date"="Some date",
 "name"="OMFGB_inc-1.3.0_nightly-5.zip",
