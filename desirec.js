fcac53d986da3f9f81ea57573c280643c8f8acca
Linuxmotion <linuxmotion@thelinuxmotion.org>
2011-06-17 21:10:42 -0700

    Add desire nightly manifest

---
+++ desirec.js
@@ -0,0 +1,11 @@
+[
+{
+"device"="DesireC",
+"base"="1.3.0",
+"version"=8,
+"date"="June 12, 2011",
+"name"="OMFGB_sholes-1.3.0_nightly-8.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/sholes/OMFGB_desirec-1.3.0_nightly-8.zip",
+"installable"="false"
+}
+]
