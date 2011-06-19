555318944bcfb2fd2aad71c27b42f7e61e878eb3
r2DoesInc <r2doesinc@gmail.com>
2011-06-19 01:59:39 -0700

    Push new nightly

--- inc.js
+++ inc.js
@@ -2,6 +2,15 @@
 {
 "device"="Incredible",
 "base"="1.3.0",
+"version"=9",
+"date"="Some date",
+"name"="OMFGB_inc-1.3.0_nightly-9.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/Inc/OMFGB_inc-1.3.0_nightly-9.zip",
+"installable"="false"
+},
+ {
+"device"="Incredible",
+"base"="1.3.0",
 "version"=8,
 "date"="Some date",
 "name"="OMFGB_inc-1.3.0_nightly-8.zip",
