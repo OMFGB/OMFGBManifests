7eefe47d4169c996694a9077261dc07ac2f923da
r2DoesInc <r2doesinc@gmail.com>
2011-06-19 22:53:10 -0700

    Edited sholes.js via GitHub

--- sholes.js
+++ sholes.js
@@ -2,10 +2,10 @@
 {
 "device"="Sholes",
 "base"="1.3.0",
-"version"=5,
-"date"="June 12, 2011",
-"name"="OMFGB_sholes-1.3.0_nightly-5.zip",
-"url"="http://r2doesinc.bitsurge.net/nightlies/sholes/OMFGB_sholes-1.3.0_nightly-5.zip",
+"version"=10,
+"date"="June 20, 2011",
+"name"="OMFGB_sholes-1.3.0_nightly-10.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/sholes/OMFGB_sholes-1.3.0_nightly-10.zip",
 "installable"="false"
 }
 ]
