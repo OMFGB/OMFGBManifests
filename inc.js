412cde4a3381d4a3060fca2e9e690822b80981e4
r2DoesInc <r2doesinc@gmail.com>
2011-06-19 19:50:53 -0700

    Edited inc.js via GitHub

--- inc.js
+++ inc.js
@@ -2,87 +2,10 @@
 {
 "device"="Incredible",
 "base"="1.3.0",
-"version"=9",
+"version"=9,
 "date"="6/19",
 "name"="OMFGB_inc-1.3.0_nightly-9.zip",
 "url"="http://r2doesinc.bitsurge.net/nightlies/Inc/OMFGB_inc-1.3.0_nightly-9.zip",
-"installable"="false"
-},
- {
-"device"="Incredible",
-"base"="1.3.0",
-"version"=8,
-"date"="Some date",
-"name"="OMFGB_inc-1.3.0_nightly-8.zip",
-"url"="http://r2doesinc.bitsurge.net/nightlies/Inc/OMFGB_inc-1.3.0_nightly-8.zip",
-"installable"="false"
-},
-{
-"device"="Incredible",
-"base"="1.3.0",
-"version"=7,
-"date"="Some date",
-"name"="OMFGB_inc-1.3.0_nightly-7.zip",
-"url"="http://r2doesinc.bitsurge.net/nightlies/Inc/OMFGB_inc-1.3.0_nightly-7.zip",
-"installable"="false"
-},
-{
-"device"="Incredible",
-"base"="1.3.0",
-"version"=6,
-"date"="Some date",
-"name"="OMFGB_inc-1.3.0_nightly-6.zip",
-"url"="http://r2doesinc.bitsurge.net/nightlies/Inc/OMFGB_inc-1.3.0_nightly-6.zip",
-"installable"="false"
-},
-
-{
-"device"="Incredible",
-"base"="1.3.0",
-"version"=5,
-"date"="Some date",
-"name"="OMFGB_inc-1.3.0_nightly-5.zip",
-"url"="http://r2doesinc.bitsurge.net/nightlies/Inc/OMFGB_inc-1.3.0_nightly-5.zip",
-"installable"="false"
-},
-
-{
-"device"="Incredible",
-"base"="1.3.0",
-"version"=4,
-"date"="Some date",
-"name"="OMFGB_inc-1.3.0_nightly-4.zip",
-"url"="http://r2doesinc.bitsurge.net/nightlies/Inc/OMFGB_inc-1.3.0_nightly-4.zip",
-"installable"="false"
-},
-
-{
-"device"="Incredible",
-"base"="1.3.0",
-"version"=3,
-"date"="Some date",
-"name"="OMFGB_inc-1.3.0_nightly-3.zip",
-"url"="http://r2doesinc.bitsurge.net/nightlies/Inc/OMFGB_inc-1.3.0_nightly-3.zip",
-"installable"="false"
-},
-
-{
-"device"="Incredible",
-"base"="1.3.0",
-"version"=2,
-"date"="Some date",
-"name"="OMFGB_inc-1.3.0_nightly-2.zip",
-"url"="http://goo.gl/Ve9n8",
-"installable"="false"
-},
-
-{
-"device"="Incredible",
-"base"="1.3.0",
-"version"=1,
-"date"="Some date",
-"name"="OMFGB_inc-1.3.0_nightly-1.zip",
-"url"="http://r2doesinc.bitsurge.net/nightlies/Inc/OMFGB_inc-1.3.0_nightly-1.zip",
 "installable"="false"
 }
 ]
