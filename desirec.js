275937cdd1e8cec9d08b0639e9e9f6cc2b12c16d
r2DoesInc <r2doesinc@gmail.com>
2011-06-20 23:00:47 -0700

    Edited desirec.js via GitHub

--- desirec.js
+++ desirec.js
@@ -4,7 +4,7 @@
 "base"="1.3.0",
 "version"=06-21,
 "date"="06-21",
-"name"="OMFGB_desirec-1.3.0_nightly--6-21.zip",
+"name"="OMFGB_desirec-1.3.0_nightly-6-21.zip",
 "url"="http://r2doesinc.bitsurge.net/nightlies/desirec/OMFGB_desirec-1.3.0_nightly-06-21.zip",
 "installable"="false"
 },
