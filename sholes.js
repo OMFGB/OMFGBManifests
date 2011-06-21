865dda08243a82efb21bd9e018385cfa3a8962f4
r2DoesInc <r2doesinc@gmail.com>
2011-06-21 01:31:22 -0500

    new nightly

--- sholes.js
+++ sholes.js
@@ -2,6 +2,15 @@
 {
 "device"="Sholes",
 "base"="1.3.0",
+"version"=06-21,
+"date"="06-21",
+"name"="OMFGB_sholes-1.3.0_nightly-6-21.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/sholes/OMFGB_sholes-1.3.0_nightly-06-21.zip",
+"installable"="false"
+},
+{
+"device"="Sholes",
+"base"="1.3.0",
 "version"=10,
 "date"="June 20, 2011",
 "name"="OMFGB_sholes-1.3.0_nightly-10.zip",
