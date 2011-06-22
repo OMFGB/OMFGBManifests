a3134433909946278df97e4649316f9bd6c1015d
r2DoesInc <r2doesinc@gmail.com>
2011-06-22 01:56:59 -0500

    New nightly

--- desirec.js
+++ desirec.js
@@ -1,4 +1,12 @@
-[
+{
+"device"="DesireC",
+"base"="1.3.0",
+"version"=06-22,
+"date"="06-22",
+"name"="OMFGB_desirec-1.3.0_nightly-6-22.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/desirec/OMFGB_desirec-1.3.0_nightly-06-22.zip",
+"installable"="false"
+},
 {
 "device"="DesireC",
 "base"="1.3.0",
