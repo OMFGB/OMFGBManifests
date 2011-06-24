4a6c441d9bb0a6521fdecaeabb22d4d398d5682b
xoomdev <xoomdevnet@gmail.com>
2011-06-24 05:36:15 -0700

    Adding 06-24 nightly for mecha. Last manual update here..

--- mecha.js
+++ mecha.js
@@ -2,6 +2,15 @@
 {
 "device"="Mecha",
 "base"="1.3.0",
+"version"=06-24,
+"date"="06-24",
+"name"="OMFGB_mecha-1.3.0-nightly-06-24.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/mecha/OMFGB_mecha-1.3.0_nightly-06-24.zip",
+"installable"="false"
+},
+{
+"device"="Mecha",
+"base"="1.3.0",
 "version"=4,
 "date"="June 11, 2011",
 "url"="http://r2doesinc.bitsurge.net/nightlies/mecha/OMFGB_mecha-1.3.0_nightly-4.zip"
