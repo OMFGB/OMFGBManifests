4d1eb8d08d6c84512ec84da6a31c3ba667548d28
linuxmotion <linuxmotion@gmail.com>
2011-06-24 17:28:27 -0700

    Add missing comma

--- mecha.js
+++ mecha.js
@@ -13,7 +13,7 @@
 "base"="1.3.0",
 "version"=4,
 "date"="June 11, 2011",
-"url"="http://r2doesinc.bitsurge.net/nightlies/mecha/OMFGB_mecha-1.3.0_nightly-4.zip"
+"url"="http://r2doesinc.bitsurge.net/nightlies/mecha/OMFGB_mecha-1.3.0_nightly-4.zip",
 "installable"="false"
 }
 ]
