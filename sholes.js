884fc4de49ded2bf25b4b491172f6c9c96f96ac6
r2DoesInc <r2doesinc@poiteeserver>
2011-06-30 06:09:20 -0400

    New 06-30 nightly for sholes. Via @OMFBOT

--- sholes.js
+++ sholes.js
@@ -2,6 +2,15 @@
 {
 "device"="sholes",
 "base"="1.3.0",
+"version"=06-30,
+"date"="06-30",
+"name"="OMFGB_sholes-1.3.0_nightly-06-30.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/sholes/OMFGB_sholes-1.3.0_nightly-06-30.zip",
+"installable"="false"
+},
+{
+"device"="sholes",
+"base"="1.3.0",
 "version"=06-29,
 "date"="06-29",
 "name"="OMFGB_sholes-1.3.0_nightly-06-29.zip",
