b1fc081187d6adcfa7ba382d533510390c915a39
r2DoesInc <r2doesinc@poiteeserver>
2011-07-01 05:15:46 -0400

    New 07-01 nightly for heroc. Via @OMFBOT

--- heroc.js
+++ heroc.js
@@ -2,6 +2,15 @@
 {
 "device"="heroc",
 "base"="1.3.0",
+"version"=07-01,
+"date"="07-01",
+"name"="OMFGB_heroc-1.3.0_nightly-07-01.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/heroc/OMFGB_heroc-1.3.0_nightly-07-01.zip",
+"installable"="false"
+},
+{
+"device"="heroc",
+"base"="1.3.0",
 "version"=06-30,
 "date"="06-30",
 "name"="OMFGB_heroc-1.3.0_nightly-06-30.zip",
