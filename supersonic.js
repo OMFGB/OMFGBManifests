197de198b25a90ec73deb650c6d14649f03333d6
r2DoesInc <r2doesinc@gmail.com>
2011-07-01 19:20:08 -0700

    Fix missing comma

--- supersonic.js
+++ supersonic.js
@@ -4,7 +4,7 @@
 "base"="1.3.0",
 "version"=4,
 "date"="Some date",
-"url"="http://r2doesinc.bitsurge.net/nightlies/supersonic/OMFGB_supersonic-1.3.0_nightly-4.zip"
+"url"="http://r2doesinc.bitsurge.net/nightlies/supersonic/OMFGB_supersonic-1.3.0_nightly-4.zip",
 "installable"="false"
 }
 ]
