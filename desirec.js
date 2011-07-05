e8c7489425ac3b996472cc020653f4f6d4041837
r2DoesInc <r2doesinc@poiteeserver>
2011-07-05 04:43:53 -0400

    New 07-05 nightly for desirec. Via @OMFBOT

--- desirec.js
+++ desirec.js
@@ -2,6 +2,15 @@
 {
 "device"="desirec",
 "base"="1.3.0",
+"version"=07-05,
+"date"="07-05",
+"name"="OMFGB_desirec-1.3.0_nightly-07-05.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/desirec/OMFGB_desirec-1.3.0_nightly-07-05.zip",
+"installable"="false"
+},
+{
+"device"="desirec",
+"base"="1.3.0",
 "version"=07-02,
 "date"="07-02",
 "name"="OMFGB_desirec-1.3.0_nightly-07-02.zip",
