a5020c0ca9e2ffc5bd328aabdaa4e7dc2fd08132
LinuxMotion <jweyrauch359@gmail.com>
2011-07-10 17:28:51 -0700

    Add a trial addons manifest

---
+++ addons.js
@@ -0,0 +1,18 @@
+[
+{
+"device"="inc",
+"name"="OMFGBk-sbc_1",
+"zipname"="OOMFGBk-sbc_1.zip",
+"url"="http://r2doesinc.bitsurge.net/Addons/OMFGBk-sbc-1.zip",
+"installable"="false",
+"description"="SBC kernel for OMFGB"
+},
+{
+"device"="evo",
+"name"="OMFGBk-sbc_1",
+"zipname"="OOMFGBk-sbc_1.zip",
+"url"="http://r2doesinc.bitsurge.net/Addons/OMFGBk-sbc-1.zip",
+"installable"="false",
+"description"="SBC kernel for OMFGB"
+}
+]
