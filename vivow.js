35dc927b5808b5b825a34cd9dcceb24e50e4938a
r2DoesInc <r2doesinc@poiteeserver>
2011-07-14 05:36:20 -0400

    New 07-14 nightly for vivow. Via @OMFBOT

--- vivow.js
+++ vivow.js
@@ -1,2 +1,12 @@
 [
+{
+"device"="vivow",
+"base"="1.3.0",
+"version"=07-14,
+"date"="07-14",
+"name"="OMFGB_vivow-1.3.0_nightly-07-14.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/vivow/OMFGB_vivow-1.3.0_nightly-07-14.zip",
+"installable"="false",
+"description"="vivow 07-14 OMFGB 1.3.0 nightly. YOU MUST WIPE."
+},
 ]
