59f9e3d06ac5304b5a491caefb18d7781f30a72f
r2DoesInc <r2doesinc@poiteeserver>
2011-07-15 08:11:27 -0400

    New 07-15 nightly for heroc. Via @OMFBOT

--- heroc.js
+++ heroc.js
@@ -2,6 +2,16 @@
 {
 "device"="heroc",
 "base"="1.3.0",
+"version"=07-15,
+"date"="07-15",
+"name"="OMFGB_heroc-1.3.0_nightly-07-15.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/heroc/OMFGB_heroc-1.3.0_nightly-07-15.zip",
+"installable"="false",
+"description"="Fixes to god mode, etc."
+},
+{
+"device"="heroc",
+"base"="1.3.0",
 "version"=07-11,
 "date"="07-11",
 "name"="OMFGB_heroc-1.3.0_nightly-07-11.zip",
