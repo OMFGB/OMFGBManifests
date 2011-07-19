e55c33084f90f58f85e9898540395e33f7597839
r2DoesInc <r2doesinc@poiteeserver>
2011-07-19 04:31:45 -0400

    New 07-19 nightly for fascinatemtd. Via @OMFBOT

--- fascinatemtd.js
+++ fascinatemtd.js
@@ -2,6 +2,16 @@
 {
 "device"="fascinatemtd",
 "base"="1.3.0",
+"version"=07-19,
+"date"="07-19",
+"name"="OMFGB_fascinatemtd-1.3.0_nightly-07-19.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/fascinatemtd/OMFGB_fascinatemtd-1.3.0_nightly-07-19.zip",
+"installable"="false",
+"description"="Fixes to god mode, etc."
+},
+{
+"device"="fascinatemtd",
+"base"="1.3.0",
 "version"=07-16,
 "date"="07-16",
 "name"="OMFGB_fascinatemtd-1.3.0_nightly-07-16.zip",
