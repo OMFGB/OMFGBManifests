4fcfc91496a48dc270a1ed96705af93380a1a6b4
MastaOfEvil <CodyPelkey901@gmail.com>
2011-07-20 16:27:34 -0500

    added mesmerizemtd and showcasemtd

---
+++ mesmerizemtd.js
@@ -0,0 +1,11 @@
+[
+{
+"device"="mesmerizemtd",
+"base"="1.3.0",
+"version"=07-20,
+"date"="07-20",
+"name"="OMFGB_mesmerizemtd-1.3.0_nightly-07-20.zip",
+"url"="http://dl.dropbox.com/u/29062346/Showcase/Roms/OMFGB_mesmerizemtd-1.3.0_nightly-07-20.zip",
+"installable"="false",
+"description"="God Mode enhancements, updated theme, new wallpaper, device specific updates, etc."
+}
---
+++ showcasemtd.js
@@ -0,0 +1,11 @@
+[
+{
+"device"="showcasemtd",
+"base"="1.3.0",
+"version"=07-20,
+"date"="07-20",
+"name"="OMFGB_showcasemtd-1.3.0_nightly-07-20.zip",
+"url"="http://dl.dropbox.com/u/29062346/Showcase/Roms/OMFGB_showcasemtd-1.3.0_nightly-07-20.zip",
+"installable"="false",
+"description"="God Mode enhancements, updated theme, new wallpaper, device specific updates, etc."
+}
