904dde29ddf03980d6c8dbbb47d5ed0f95bf9151
r2DoesInc <r2doesinc@poiteeserver>
2011-07-23 08:40:13 -0400

    New 07-23 nightly for sholes. Via @OMFBOT

--- sholes.js
+++ sholes.js
@@ -2,6 +2,16 @@
 {
 "device"="sholes",
 "base"="1.3.0",
+"version"=07-23,
+"date"="07-23",
+"name"="OMFGB_sholes-1.3.0_nightly-07-23.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/sholes/OMFGB_sholes-1.3.0_nightly-07-23.zip",
+"installable"="false",
+"description"="You MUST manually wipe /system BEFORE flashing tonights build. God Mode enhancements and fixes, device specific updates -wimax and camera, updated google apps."
+},
+{
+"device"="sholes",
+"base"="1.3.0",
 "version"=07-21,
 "date"="07-21",
 "name"="OMFGB_sholes-1.3.0_nightly-07-21.zip",
