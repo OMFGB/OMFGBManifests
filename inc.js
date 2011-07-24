40ea75b3e6e6f9b31a3b3dbca48937ba3a36e386
r2DoesInc <r2doesinc@poiteeserver>
2011-07-24 02:41:45 -0400

    New 07-24 nightly for inc. Via @OMFBOT

--- inc.js
+++ inc.js
@@ -2,6 +2,16 @@
 {
 "device"="inc",
 "base"="1.3.0",
+"version"=07-24,
+"date"="07-24",
+"name"="OMFGB_inc-1.3.0_nightly-07-24.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/inc/OMFGB_inc-1.3.0_nightly-07-24.zip",
+"installable"="false",
+"description"="We did stuff."
+},
+{
+"device"="inc",
+"base"="1.3.0",
 "version"=07-23,
 "date"="07-23",
 "name"="OMFGB_inc-1.3.0_nightly-07-23.zip",
