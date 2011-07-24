ec3c298f68732ccd032fb43171fbb0a3bfbfb502
r2DoesInc <r2doesinc@poiteeserver>
2011-07-24 05:25:02 -0400

    New 07-24 nightly for supersonic. Via @OMFBOT

--- supersonic.js
+++ supersonic.js
@@ -2,6 +2,16 @@
 {
 "device"="supersonic",
 "base"="1.3.0",
+"version"=07-24,
+"date"="07-24",
+"name"="OMFGB_supersonic-1.3.0_nightly-07-24.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/supersonic/OMFGB_supersonic-1.3.0_nightly-07-24.zip",
+"installable"="false",
+"description"="We did stuff."
+},
+{
+"device"="supersonic",
+"base"="1.3.0",
 "version"=07-23,
 "date"="07-23",
 "name"="OMFGB_supersonic-1.3.0_nightly-07-23.zip",
