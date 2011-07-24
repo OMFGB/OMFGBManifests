61a77b5f256643bf87735760bcf1aa43d68a0f7e
MastaOfEvil <CodyPelkey901@gmail.com>
2011-07-24 12:35:00 -0500

    Updated to 7/24 Nightly

--- mesmerizemtd.js
+++ mesmerizemtd.js
@@ -2,11 +2,41 @@
 {
 "device"="mesmerizemtd",
 "base"="1.3.0",
+"version"=07-24,
+"date"="07-24",
+"name"="OMFGB_mesmerizemtd-1.3.0_nightly-07-24.zip",
+"url"="http://dl.dropbox.com/u/29062346/Showcase/Roms/OMFGB_mesmerizemtd-1.3.0_nightly-07-24.zip",
+"installable"="false",
+"description"="We did stuff."
+},
+{
+"device"="mesmerizemtd",
+"base"="1.3.0",
+"version"=07-21,
+"date"="07-21",
+"name"="OMFGB_mesmerizemtd-1.3.0_nightly-07-21.zip",
+"url"="http://dl.dropbox.com/u/29062346/Showcase/Roms/OMFGB_mesmerizemtd-1.3.0_nightly-07-21.zip",
+"installable"="false",
+"description"="You MUST manually wipe /system BEFORE flashing tonights build. God Mode enhancements and fixes, device specific updates -wimax and camera, updated google apps."
+},
+{
+"device"="mesmerizemtd",
+"base"="1.3.0",
 "version"=07-20,
 "date"="07-20",
 "name"="OMFGB_mesmerizemtd-1.3.0_nightly-07-20.zip",
 "url"="http://dl.dropbox.com/u/29062346/Showcase/Roms/OMFGB_mesmerizemtd-1.3.0_nightly-07-20.zip",
 "installable"="false",
 "description"="God Mode enhancements, updated theme, new wallpaper, device specific updates, etc."
-}
+},
+{
+"device"="mesmerizemtd",
+"base"="1.3.0",
+"version"=07-19,
+"date"="07-19",
+"name"="OMFGB_mesmerizemtd-1.3.0_nightly-07-19.zip",
+"url"="http://dl.dropbox.com/u/29062346/Showcase/Roms/OMFGB_mesmerizemtd-1.3.0_nightly-07-19.zip",
+"installable"="false",
+"description"=""
+},
 ]
--- showcasemtd.js
+++ showcasemtd.js
@@ -2,11 +2,41 @@
 {
 "device"="showcasemtd",
 "base"="1.3.0",
+"version"=07-24,
+"date"="07-24",
+"name"="OMFGB_showcasemtd-1.3.0_nightly-07-24.zip",
+"url"="http://dl.dropbox.com/u/29062346/Showcase/Roms/OMFGB_showcasemtd-1.3.0_nightly-07-24.zip",
+"installable"="false",
+"description"="We did stuff."
+},
+{
+"device"="showcasemtd",
+"base"="1.3.0",
+"version"=07-21,
+"date"="07-21",
+"name"="OMFGB_showcasemtd-1.3.0_nightly-07-21.zip",
+"url"="http://dl.dropbox.com/u/29062346/Showcase/Roms/OMFGB_showcasemtd-1.3.0_nightly-07-21.zip",
+"installable"="false",
+"description"="You MUST manually wipe /system BEFORE flashing tonights build. God Mode enhancements and fixes, device specific updates -wimax and camera, updated google apps."
+},
+{
+"device"="showcasemtd",
+"base"="1.3.0",
 "version"=07-20,
 "date"="07-20",
 "name"="OMFGB_showcasemtd-1.3.0_nightly-07-20.zip",
 "url"="http://dl.dropbox.com/u/29062346/Showcase/Roms/OMFGB_showcasemtd-1.3.0_nightly-07-20.zip",
 "installable"="false",
 "description"="God Mode enhancements, updated theme, new wallpaper, device specific updates, etc."
-}
+},
+{
+"device"="showcasemtd",
+"base"="1.3.0",
+"version"=07-19,
+"date"="07-19",
+"name"="OMFGB_showcasemtd-1.3.0_nightly-07-19.zip",
+"url"="http://dl.dropbox.com/u/29062346/Showcase/Roms/OMFGB_showcasemtd-1.3.0_nightly-07-19.zip",
+"installable"="false",
+"description"=""
+},
 ]
