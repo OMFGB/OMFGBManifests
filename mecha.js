10c38b056d49b4475c0c93858bac48c4937db188
r2DoesInc <r2doesinc@poiteeserver>
2011-07-30 03:18:13 -0400

    New 07-30 nightly for mecha. Via @OMFBOT

--- mecha.js
+++ mecha.js
@@ -2,6 +2,16 @@
 {
 "device"="mecha",
 "base"="1.3.1",
+"version"=07-30,
+"date"="07-30",
+"name"="OMFGB_mecha-1.3.1_nightly-07-30.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/mecha/OMFGB_mecha-1.3.1_nightly-07-30.zip",
+"installable"="false",
+"description"="4G LTE Toggle included. Broken MMS fixed."
+},
+{
+"device"="mecha",
+"base"="1.3.1",
 "version"=07-29,
 "date"="07-29",
 "name"="OMFGB_mecha-1.3.1_nightly-07-29.zip",
