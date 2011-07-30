f7757e1f254e916ed2426d52b60df7131726ae67
r2DoesInc <r2doesinc@poiteeserver>
2011-07-30 03:58:26 -0400

    New 07-30 nightly for vivow. Via @OMFBOT

--- vivow.js
+++ vivow.js
@@ -2,6 +2,16 @@
 {
 "device"="vivow",
 "base"="1.3.1",
+"version"=07-30,
+"date"="07-30",
+"name"="OMFGB_vivow-1.3.1_nightly-07-30.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/vivow/OMFGB_vivow-1.3.1_nightly-07-30.zip",
+"installable"="false",
+"description"="4G LTE Toggle included. Broken MMS fixed."
+},
+{
+"device"="vivow",
+"base"="1.3.1",
 "version"=07-29,
 "date"="07-29",
 "name"="OMFGB_vivow-1.3.1_nightly-07-29.zip",
