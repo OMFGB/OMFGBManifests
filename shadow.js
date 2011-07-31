c37593d27e85d1256e997cd9f66101337a27f1ac
r2DoesInc <r2doesinc@poiteeserver>
2011-07-31 08:23:46 -0400

    New 07-31 nightly for shadow. Via @OMFBOT

--- shadow.js
+++ shadow.js
@@ -2,6 +2,16 @@
 {
 "device"="shadow",
 "base"="1.3.1",
+"version"=07-31,
+"date"="07-31",
+"name"="OMFGB_shadow-1.3.1_nightly-07-31.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/shadow/OMFGB_shadow-1.3.1_nightly-07-31.zip",
+"installable"="false",
+"description"="Droid X added. Added 1% battery increments since last DX beta. New Launcher speed tweak from Jt1134. In call audio tweaks to Tunderbolt. Fix Fascinate wifi?"
+},
+{
+"device"="shadow",
+"base"="1.3.1",
 "version"=07-30,
 "date"="07-30",
 "name"="OMFGB_mecha-1.3.1_nightly-07-30.zip",
