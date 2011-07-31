46658743f820ae41e0bc1ff1e5dbb397854b249e
r2DoesInc <r2doesinc@poiteeserver>
2011-07-31 05:25:13 -0400

    Add shadow js

---
+++ shadow.js
@@ -0,0 +1,13 @@
+[
+{
+"device"="shadow",
+"base"="1.3.1",
+"version"=07-30,
+"date"="07-30",
+"name"="OMFGB_mecha-1.3.1_nightly-07-30.zip",
+"url"="NA",
+"installable"="false",
+"description"="This is me trolling you."
+}
+]
+
