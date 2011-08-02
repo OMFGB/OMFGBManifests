e417705eb6e8f28bdf2385a6fc59f40fea1d0c14
r2DoesInc <r2doesinc@poiteeserver>
2011-08-02 08:02:58 -0400

    New 08-02 nightly for mesmerizemtd. Via @OMFBOT

--- mesmerizemtd.js
+++ mesmerizemtd.js
@@ -2,6 +2,16 @@
 {
 "device"="mesmerizemtd",
 "base"="1.3.1",
+"version"=08-02,
+"date"="08-02",
+"name"="OMFGB_mesmerizemtd-1.3.1_nightly-08-02.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/mesmerizemtd/OMFGB_mesmerizemtd-1.3.1_nightly-08-02.zip",
+"installable"="false",
+"description"="MIUI Battery color changing, Google apps flashing option, Droid X fixes, Netflix fixed."
+},
+{
+"device"="mesmerizemtd",
+"base"="1.3.1",
 "version"=07-31,
 "date"="07-31",
 "name"="OMFGB_mesmerizemtd-1.3.1_nightly-07-31.zip",
