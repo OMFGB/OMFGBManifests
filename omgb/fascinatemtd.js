6bb41971a95311f1efb097135ccdb16fad1df6b8
r2DoesInc <r2doesinc@poiteeserver>
2011-08-05 07:58:28 -0400

    .

--- omgb/fascinatemtd.js
+++ omgb/fascinatemtd.js
@@ -1,8 +1,8 @@
 [
 {
 "device"="fascinatemtd",
-"version"=1.2.2,
-"date"="04-20,
+"version"="1.2.2",
+"date"="04-20",
 "name"="OMFGB_fascinatemtd-1.3.1_nightly-08-02.zip",
 "url"="http://r2doesinc.bitsurge.net/nightlies/fascinatemtd/OMFGB_fascinatemtd-1.3.1_nightly-08-02.zip",
 "installable"="false",
