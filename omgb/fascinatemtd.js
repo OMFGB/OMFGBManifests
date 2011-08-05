396cc96810855a9c704dc0873239bb5dcea33113
r2DoesInc <r2doesinc@poiteeserver>
2011-08-05 06:58:16 -0400

    omgb manifests

---
+++ omgb/fascinatemtd.js
@@ -0,0 +1,12 @@
+[
+{
+"device"="fascinatemtd",
+"base"="1.3.1",
+"version"=08-02,
+"date"="08-02",
+"name"="OMFGB_fascinatemtd-1.3.1_nightly-08-02.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/fascinatemtd/OMFGB_fascinatemtd-1.3.1_nightly-08-02.zip",
+"installable"="false",
+"description"="MIUI Battery color changing, Google apps flashing option, Droid X fixes, Netflix fixed."
+}
+]
