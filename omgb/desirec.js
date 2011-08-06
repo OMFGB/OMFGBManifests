80214ad0c401937e7a55c16af0b99f2c45e55bdf
r2DoesInc <r2doesinc@poiteeserver>
2011-08-06 07:58:42 -0400

    New OMGB build for desirec. Via @OMFBOT

--- omgb/desirec.js
+++ omgb/desirec.js
@@ -1,3 +1,12 @@
 [
+{
+"device"="desirec",
+"version"="1.2.2",
+"date"="08-06",
+"name"="OMGB_desirec-1.2.2.zip",
+"url"="http://r2doesinc.bitsurge.net/omgb/desirec/OMGB_desirec-1.2.2.zip",
+"installable"="false",
+"description"="The long awaited 1.2.2. Check in rom changelog."
+},
  
 ]
