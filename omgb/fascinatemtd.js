cb6a7a0033d69faf6f304147c83c377bab6b6184
r2DoesInc <r2doesinc@gmail.com>
2011-08-06 07:32:45 -0300

    Edited omgb/fascinatemtd.js via GitHub

--- omgb/fascinatemtd.js
+++ omgb/fascinatemtd.js
@@ -1,10 +1,10 @@
 [
 {
-"device"="",
+"device"="fascinatemtd",
 "version"="1.2.2",
 "date"="08-06",
-"name"="",
-"url"="http://r2doesinc.bitsurge.net/omgb//",
+"name"="OMGB_fascinatemtd-1.2.2.zip",
+"url"="http://r2doesinc.bitsurge.net/omgb/fascinate/OMGB_fascinatemtd-1.2.2.zip",
 "installable"="false",
 "description"="The long awaited 1.2.2. Check in rom changelog."
 },
