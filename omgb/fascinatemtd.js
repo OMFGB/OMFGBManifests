07edacffc4ee88ea32e0d3d6f11cd79b23042b88
r2DoesInc <r2doesinc@poiteeserver>
2011-08-06 02:41:19 -0400

    New OMGB build for fascinatemtd. Via @OMFBOT

--- omgb/fascinatemtd.js
+++ omgb/fascinatemtd.js
@@ -1,3 +1,12 @@
 [
+{
+"device"="",
+"version"="1.2.2",
+"date"="08-06",
+"name"="",
+"url"="http://r2doesinc.bitsurge.net/omgb//",
+"installable"="false",
+"description"="The long awaited 1.2.2. Check in rom changelog."
+},
  
 ]
