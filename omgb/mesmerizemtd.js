6cb83752c33653d44d43567003c95b163a7b1fc1
r2DoesInc <r2doesinc@poiteeserver>
2011-08-06 10:57:07 -0400

    New OMGB build for mesmerizemtd. Via @OMFBOT

--- omgb/mesmerizemtd.js
+++ omgb/mesmerizemtd.js
@@ -1,3 +1,12 @@
 [
+{
+"device"="mesmerizemtd",
+"version"="1.2.2",
+"date"="08-06",
+"name"="OMGB_mesmerizemtd-1.2.2.zip",
+"url"="http://r2doesinc.bitsurge.net/omgb/mesmerizemtd/OMGB_mesmerizemtd-1.2.2.zip",
+"installable"="false",
+"description"="The long awaited 1.2.2. Check in rom changelog."
+},
  
 ]
