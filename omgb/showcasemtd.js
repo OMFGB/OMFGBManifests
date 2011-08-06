12992ed8e88db0021f356d7a439ca00214764669
r2DoesInc <r2doesinc@poiteeserver>
2011-08-06 11:49:09 -0400

    New OMGB build for showcasemtd. Via @OMFBOT

--- omgb/showcasemtd.js
+++ omgb/showcasemtd.js
@@ -1,3 +1,12 @@
 [
+{
+"device"="showcasemtd",
+"version"="1.2.2",
+"date"="08-06",
+"name"="OMGB_showcasemtd-1.2.2.zip",
+"url"="http://r2doesinc.bitsurge.net/omgb/showcasemtd/OMGB_showcasemtd-1.2.2.zip",
+"installable"="false",
+"description"="The long awaited 1.2.2. Check in rom changelog."
+},
  
 ]
