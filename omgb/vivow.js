c68cc709cd83880bcca121c428049d2e0d6dd5b0
r2DoesInc <r2doesinc@poiteeserver>
2011-08-06 05:36:47 -0400

    New OMGB build for vivow. Via @OMFBOT

--- omgb/vivow.js
+++ omgb/vivow.js
@@ -1,3 +1,12 @@
 [
+{
+"device"="vivow",
+"version"="1.2.2",
+"date"="08-06",
+"name"="OMGB_vivow-1.2.2.zip",
+"url"="http://r2doesinc.bitsurge.net/omgb/vivow/OMGB_vivow-1.2.2.zip",
+"installable"="false",
+"description"="The long awaited 1.2.2. Check in rom changelog."
+},
  
 ]
