38d99d0e265080322fb9c3bfaa0947986d0bc0e3
r2DoesInc <r2doesinc@poiteeserver>
2011-08-06 06:26:01 -0400

    New OMGB build for supersonic. Via @OMFBOT

--- omgb/supersonic.js
+++ omgb/supersonic.js
@@ -1,3 +1,12 @@
 [
+{
+"device"="supersonic",
+"version"="1.2.2",
+"date"="08-06",
+"name"="OMGB_supersonic-1.2.2.zip",
+"url"="http://r2doesinc.bitsurge.net/omgb/supersonic/OMGB_supersonic-1.2.2.zip",
+"installable"="false",
+"description"="The long awaited 1.2.2. Check in rom changelog."
+},
  
 ]
