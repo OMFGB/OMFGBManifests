e9e0de00c69a011d9828160fcf2e7e2a21105b96
r2DoesInc <r2doesinc@poiteeserver>
2011-08-06 07:13:46 -0400

    New OMGB build for sholes. Via @OMFBOT

--- omgb/sholes.js
+++ omgb/sholes.js
@@ -1,3 +1,12 @@
 [
+{
+"device"="sholes",
+"version"="1.2.2",
+"date"="08-06",
+"name"="OMGB_sholes-1.2.2.zip",
+"url"="http://r2doesinc.bitsurge.net/omgb/sholes/OMGB_sholes-1.2.2.zip",
+"installable"="false",
+"description"="The long awaited 1.2.2. Check in rom changelog."
+},
  
 ]
