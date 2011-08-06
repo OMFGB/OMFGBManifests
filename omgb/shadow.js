729257ee25b4dc4cfa718bd8711ae33b689c0588
r2DoesInc <r2doesinc@poiteeserver>
2011-08-06 03:57:55 -0400

    New OMGB build for shadow. Via @OMFBOT

--- omgb/shadow.js
+++ omgb/shadow.js
@@ -1,3 +1,12 @@
 [
+{
+"device"="shadow",
+"version"="1.2.2",
+"date"="08-06",
+"name"="OMGB_shadow-1.2.2.zip",
+"url"="http://r2doesinc.bitsurge.net/omgb/shadow/OMGB_shadow-1.2.2.zip",
+"installable"="false",
+"description"="The long awaited 1.2.2. Check in rom changelog."
+},
  
 ]
