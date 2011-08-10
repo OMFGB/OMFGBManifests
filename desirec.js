e8b061e6ad7a6a85ab219d2948dd5985ea5abc95
r2DoesInc <r2doesinc@poiteeserver>
2011-08-10 07:19:30 -0400

    New 08-10 nightly for desirec. Via @OMFBOT

--- desirec.js
+++ desirec.js
@@ -2,6 +2,16 @@
 {
 "device"="desirec",
 "base"="1.3.1",
+"version"=08-10,
+"date"="08-10",
+"name"="OMFGB_desirec-1.3.1_nightly-08-10.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/desirec/OMFGB_desirec-1.3.1_nightly-08-10.zip",
+"installable"="false",
+"description"="Cleaned up a ton of code and fix issues with the lockscreens and batteries in God Mode."
+},
+{
+"device"="desirec",
+"base"="1.3.1",
 "version"=08-08,
 "date"="08-08",
 "name"="OMFGB_desirec-1.3.1_nightly-08-08.zip",
