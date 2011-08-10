dba7291db7d35fcbdb86f7f48bf729ca253f93bd
r2DoesInc <r2doesinc@poiteeserver>
2011-08-10 02:37:11 -0400

    New 08-10 nightly for fascinatemtd. Via @OMFBOT

--- fascinatemtd.js
+++ fascinatemtd.js
@@ -2,6 +2,16 @@
 {
 "device"="fascinatemtd",
 "base"="1.3.1",
+"version"=08-10,
+"date"="08-10",
+"name"="OMFGB_fascinatemtd-1.3.1_nightly-08-10.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/fascinatemtd/OMFGB_fascinatemtd-1.3.1_nightly-08-10.zip",
+"installable"="false",
+"description"="Cleaned up a ton of code and fix issues with the lockscreens and batteries in God Mode."
+},
+{
+"device"="fascinatemtd",
+"base"="1.3.1",
 "version"=08-08,
 "date"="08-08",
 "name"="OMFGB_fascinatemtd-1.3.1_nightly-08-08.zip",
