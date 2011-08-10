1b21a02d33801847a0161d6e90594f9c74b26bd5
r2DoesInc <r2doesinc@poiteeserver>
2011-08-10 05:23:14 -0400

    New 08-10 nightly for vivow. Via @OMFBOT

--- vivow.js
+++ vivow.js
@@ -2,6 +2,16 @@
 {
 "device"="vivow",
 "base"="1.3.1",
+"version"=08-10,
+"date"="08-10",
+"name"="OMFGB_vivow-1.3.1_nightly-08-10.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/vivow/OMFGB_vivow-1.3.1_nightly-08-10.zip",
+"installable"="false",
+"description"="Cleaned up a ton of code and fix issues with the lockscreens and batteries in God Mode."
+},
+{
+"device"="vivow",
+"base"="1.3.1",
 "version"=08-08,
 "date"="08-08",
 "name"="OMFGB_vivow-1.3.1_nightly-08-08.zip",
