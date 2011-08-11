afaa327b28f1cdab22b6432d41ac10744aba8a50
r2DoesInc <r2doesinc@poiteeserver>
2011-08-11 04:49:22 -0400

    New 08-11 nightly for inc. Via @OMFBOT

--- inc.js
+++ inc.js
@@ -2,6 +2,16 @@
 {
 "device"="inc",
 "base"="1.3.1",
+"version"=08-11,
+"date"="08-11",
+"name"="OMFGB_inc-1.3.1_nightly-08-11.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/inc/OMFGB_inc-1.3.1_nightly-08-11.zip",
+"installable"="false",
+"description"="Cleaned up a ton of code and fix issues with the lockscreens and batteries in God Mode. Update kernel for MUCH louder in call audio for mecha. Many AOSP updates."
+},
+{
+"device"="inc",
+"base"="1.3.1",
 "version"=08-10,
 "date"="08-10",
 "name"="OMFGB_inc-1.3.1_nightly-08-10.zip",
