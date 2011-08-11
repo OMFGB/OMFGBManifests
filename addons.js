97601e3ad204630916655d26fa9f6e8b2569591e
Shane Milton <Shane@IndyALT.NET>
2011-08-11 03:26:36 -0300

    Added a ton of themes and updated kernels. Download stats are available upon request when hosted by files.jaxidian.org. NOTE: There could be a syntax error in this. I was unable to test this file prior to submitting the pull request, so please provide some basic level of testing prior to approving this! Lastly, I received permission from all of these devs to post these files. Tiamat's permission was conditional of being able to provide download stats, which is now possible.

--- addons.js
+++ addons.js
@@ -42,6 +42,106 @@
 "density"="hdpi",
 "description"="Custom theme designed for OMFGB and OMGB."
 },
+{
+"device"="all",
+"name"="Azure",
+"zipname"="AzureHDPI.apk",
+"url"="http://files.jaxidian.org/Android/download.php?file=10",
+"installable"="true",
+"category"="themes",
+"density"="hdpi",
+"description"="Azure by Arzbhatia is a minimalist gray theme. More info: http://l.jaxidian.org/AzureTheme"
+},
+{
+"device"="all",
+"name"="Black Funk",
+"zipname"="BlackFunk_hdpi.apk",
+"url"="http://files.jaxidian.org/Android/download.php?file=6",
+"installable"="true",
+"category"="themes",
+"density"="hdpi",
+"description"="Black Funk by Kfunk is a glassy white on black theme. More info: http://l.jaxidian.org/BlackFunkTheme"
+},
+{
+"device"="all",
+"name"="Dark Horse",
+"zipname"="darkhorse-hdpi.apk",
+"url"="http://files.jaxidian.org/Android/download.php?file=7",
+"installable"="true",
+"category"="themes",
+"density"="hdpi",
+"description"="Dark Horse by Kfunk is a glassy blue on black theme. More info: http://l.jaxidian.org/DarkHorseTheme"
+},
+{
+"device"="all",
+"name"="Indistinct (w/ batt %)",
+"zipname"="Indistinct3WithBatteryPercentage.apk",
+"url"="http://files.jaxidian.org/Android/download.php?file=8",
+"installable"="true",
+"category"="themes",
+"density"="hdpi",
+"description"="Indistinct (w/ batt %) by Arzbhatia is a simple, low-key black-based theme. More info: http://l.jaxidian.org/IndistinctTheme"
+},
+{
+"device"="all",
+"name"="Indistinct (w/o batt %)",
+"zipname"="Indistinct3WithoutBatteryPercentage.apk",
+"url"="http://files.jaxidian.org/Android/download.php?file=9",
+"installable"="true",
+"category"="themes",
+"density"="hdpi",
+"description"="Indistinct (w/o batt %) by Arzbhatia is a simple, low-key black-based theme. More info: http://l.jaxidian.org/IndistinctTheme"
+},
+{
+"device"="all",
+"name"="Whiteout",
+"zipname"="signedNEG_Whiteout_beta_1.3.9.apk",
+"url"="http://files.jaxidian.org/Android/download.php?file=5",
+"installable"="true",
+"category"="themes",
+"density"="hdpi",
+"description"="Whiteout by XsMagical (NewEraGFx) is a white-based theme. More info: http://l.jaxidian.org/WhiteoutTheme"
+},
+{
+"device"="all",
+"name"="X-Series (Blue)",
+"zipname"="BlueX.apk",
+"url"="http://files.jaxidian.org/Android/download.php?file=11",
+"installable"="true",
+"category"="themes",
+"density"="hdpi",
+"description"="X-Series (Blue) by Natemz is a blue on black theme. More info: http://l.jaxidian.org/TheXSeries"
+},
+{
+"device"="all",
+"name"="X-Series (Green)",
+"zipname"="GreenX.apk",
+"url"="http://files.jaxidian.org/Android/download.php?file=12",
+"installable"="true",
+"category"="themes",
+"density"="hdpi",
+"description"="X-Series (Green) by Natemz is a green on black theme. More info: http://l.jaxidian.org/TheXSeries"
+},
+{
+"device"="all",
+"name"="X-Series (Orange)",
+"zipname"="OrangeX.apk",
+"url"="http://files.jaxidian.org/Android/download.php?file=13",
+"installable"="true",
+"category"="themes",
+"density"="hdpi",
+"description"="X-Series (Orange) by Natemz is an orange on black theme. More info: http://l.jaxidian.org/TheXSeries"
+},
+{
+"device"="all",
+"name"="X-Series (Red)",
+"zipname"="RedX.apk",
+"url"="http://files.jaxidian.org/Android/download.php?file=14",
+"installable"="true",
+"category"="themes",
+"density"="hdpi",
+"description"="X-Series (Red) by Natemz is a red on black theme. More info: http://l.jaxidian.org/TheXSeries"
+},
 
 //Kernels
 /////Incredible
@@ -72,16 +172,25 @@
 "url"="http://crackflashers.com/DRod2169/Thunderbolt/Kernels/KangBang-CFS-0.9.zip",
 "installable"="false",
 "category"="kernel",
-"description"="KangBang! CFS v-0.9 by drod2169"
+"description"="KangBang! CFS v-0.9 by drod2169. More info: http://l.jaxidian.org/KangBangKernel"
 },
 {
 "device"="mecha",
-"name"="KangBang! BFS v-0.9",
-"zipname"="KangBang-BFS-0.9.zip",
-"url"="http://crackflashers.com/DRod2169/Thunderbolt/Kernels/KangBang-BFS-0.9.zip",
+"name"="KangBang! BFS v-1.0",
+"zipname"="KangBang-1.0-BFS.zip",
+"url"="http://files.jaxidian.org/Android/download.php?file=4",
 "installable"="false",
 "category"="kernel",
-"description"="KangBang! BFS v-0.9 by drod2169"
+"description"="KangBang! BFS v-1.0 by drod2169. More info: http://l.jaxidian.org/KangBangKernel"
+},
+{
+"device"="mecha",
+"name"="leanKernel v-3.5.11 TEST",
+"zipname"="imoseyon_leanKernel_v3.5.11AOSPtest.zip",
+"url"="http://files.jaxidian.org/Android/download.php?file=3",
+"installable"="false",
+"category"="kernel",
+"description"="leanKernel v-3.5.11 TEST by imoseyon. More info: http://l.jaxidian.org/LeanKernel"
 },
 {
 "device"="mecha",
@@ -90,7 +199,7 @@
 "url"="http://www.androiddoes.net/~imoseyon/imoseyon_leanKernel_v3.5.8AOSP.zip",
 "installable"="false",
 "category"="kernel",
-"description"="leanKernel v-3.5.8 by imoseyon"
+"description"="leanKernel v-3.5.8 by imoseyon. More info: http://l.jaxidian.org/LeanKernel"
 },
 {
 "device"="mecha",
@@ -99,7 +208,16 @@
 "url"="http://www.androiddoes.net/~imoseyon/imoseyon_leanKernel_184Mhz_v3.5.8AOSP.zip",
 "installable"="false",
 "category"="kernel",
-"description"="leanKernel v-3.5.8 (unlocks 184MHz but is unstable and overheats for some) by imoseyon"
+"description"="leanKernel v-3.5.8 (unlocks 184MHz but is unstable and overheats for some) by imoseyon. More info: http://l.jaxidian.org/LeanKernel"
+},
+{
+"device"="mecha",
+"name"="Tiamat v1.1.0",
+"zipname"="Tiamat_Mecha-v1.1.0.zip",
+"url"="http://files.jaxidian.org/Android/download.php?file=15",
+"installable"="false",
+"category"="kernel",
+"description"="Tiamat v1.1.0 by Cayniarb. More info: http://tiamat-dev.com"
 }
 ]
 
