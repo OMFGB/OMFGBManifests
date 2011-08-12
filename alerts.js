7dd37400649193b0bfddad6b4f4c02f5680a4d9b
r2DoesInc <r2doesinc@poiteeserver>
2011-08-12 03:44:45 -0400

    Alerts manfest

---
+++ alerts.js
@@ -0,0 +1,12 @@
+[
+//Hacky, to be optimized at a later time.
+{
+"device"="all",
+"name"="Hello Everyone!",
+"zipname"="N/A",
+"url"="N/A",
+"installable"="false",
+"category"="google",
+"description"="Welcome to the rom. This part is in development. REALLY cool things coming though."
+}
+]
