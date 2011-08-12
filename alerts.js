5b946056696c92483b6d3a9ccad751c10fa62d04
r2DoesInc <r2doesinc@poiteeserver>
2011-08-12 04:07:41 -0400

    Update alert

--- alerts.js
+++ alerts.js
@@ -3,10 +3,7 @@
 {
 "device"="all",
 "name"="Hello Everyone!",
-"zipname"="N/A",
-"url"="N/A",
-"installable"="false",
-"category"="google",
-"description"="Welcome to the rom. This part is in development. REALLY cool things coming though."
+"category"="r2doesinc",
+"description"="Welcome to the rom. This part is in development. REALLY cool things coming though. PS, dont click me or you get an FC."
 }
 ]
