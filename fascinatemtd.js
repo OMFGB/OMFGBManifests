19c82338dd4a57babc5f974a6b2da30add5ac3a8
r2DoesInc <r2doesinc@poiteeserver>
2011-08-12 06:18:20 -0400

    New 08-12 nightly for fascinatemtd. Via @OMFBOT

--- fascinatemtd.js
+++ fascinatemtd.js
@@ -2,6 +2,16 @@
 {
 "device"="fascinatemtd",
 "base"="1.3.1",
+"version"=08-12,
+"date"="08-12",
+"name"="OMFGB_fascinatemtd-1.3.1_nightly-08-12.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/fascinatemtd/OMFGB_fascinatemtd-1.3.1_nightly-08-12.zip",
+"installable"="false",
+"description"="Fixed broken usb mounting from last night. More God Mode updates to clean and organize settings. Totally brand new "Bulletin Board" concept in God Mode."
+},
+{
+"device"="fascinatemtd",
+"base"="1.3.1",
 "version"=08-11,
 "date"="08-11",
 "name"="OMFGB_fascinatemtd-1.3.1_nightly-08-11.zip",
