de3c6ec9e1b265806c049a0d4976958f6e42a8ca
r2DoesInc <r2doesinc@poiteeserver>
2011-08-12 05:31:12 -0400

    New 08-12 nightly for mecha. Via @OMFBOT

--- mecha.js
+++ mecha.js
@@ -2,6 +2,16 @@
 {
 "device"="mecha",
 "base"="1.3.1",
+"version"=08-12,
+"date"="08-12",
+"name"="OMFGB_mecha-1.3.1_nightly-08-12.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/mecha/OMFGB_mecha-1.3.1_nightly-08-12.zip",
+"installable"="false",
+"description"="Fixed broken usb mounting from last night. More God Mode updates to clean and organize settings. Totally brand new "Bulletin Board" concept in God Mode."
+},
+{
+"device"="mecha",
+"base"="1.3.1",
 "version"=08-11,
 "date"="08-11",
 "name"="OMFGB_mecha-1.3.1_nightly-08-11.zip",
