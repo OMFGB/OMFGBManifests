d5ef41d4b8c279f34a60da163af065969e1fd46e
r2DoesInc <r2doesinc@poiteeserver>
2011-08-12 08:21:59 -0400

    New 08-12 nightly for vivow. Via @OMFBOT

--- vivow.js
+++ vivow.js
@@ -2,6 +2,16 @@
 {
 "device"="vivow",
 "base"="1.3.1",
+"version"=08-12,
+"date"="08-12",
+"name"="OMFGB_vivow-1.3.1_nightly-08-12.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/vivow/OMFGB_vivow-1.3.1_nightly-08-12.zip",
+"installable"="false",
+"description"="Fixed broken usb mounting from last night. More God Mode updates to clean and organize settings. Totally brand new "Bulletin Board" concept in God Mode."
+},
+{
+"device"="vivow",
+"base"="1.3.1",
 "version"=08-11,
 "date"="08-11",
 "name"="OMFGB_vivow-1.3.1_nightly-08-11.zip",
