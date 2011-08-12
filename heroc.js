375323b524393472832168a4d1c5c3d01c9de221
r2DoesInc <r2doesinc@poiteeserver>
2011-08-12 11:02:03 -0400

    New 08-12 nightly for heroc. Via @OMFBOT

--- heroc.js
+++ heroc.js
@@ -2,6 +2,16 @@
 {
 "device"="heroc",
 "base"="1.3.1",
+"version"=08-12,
+"date"="08-12",
+"name"="OMFGB_heroc-1.3.1_nightly-08-12.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/heroc/OMFGB_heroc-1.3.1_nightly-08-12.zip",
+"installable"="false",
+"description"="Fixed broken usb mounting from last night. More God Mode updates to clean and organize settings. Totally brand new "Bulletin Board" concept in God Mode."
+},
+{
+"device"="heroc",
+"base"="1.3.1",
 "version"=08-11,
 "date"="08-11",
 "name"="OMFGB_heroc-1.3.1_nightly-08-11.zip",
