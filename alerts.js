b03d53888964710c2974391c6d5076e599f5da7f
r2DoesInc <r2doesinc@poiteeserver>
2011-08-12 05:56:35 -0400

    update

--- alerts.js
+++ alerts.js
@@ -4,5 +4,11 @@
 "name"="Hello Everyone!",
 "category"="r2doesinc",
 "description"="Welcome to the rom. This part is in development. REALLY cool things coming though."
+},
+{
+"device"="all",
+"name"="Whats is this?",
+"category"="r2doesinc",
+"description"="This is a section for us to post notes, updated in real time, to the users. I have plans for user replies to be implemented at a later time."
 }
 ]
