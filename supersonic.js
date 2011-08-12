59bb482711e7b0d5201b03421d54203b726a623d
r2DoesInc <r2doesinc@poiteeserver>
2011-08-12 09:04:13 -0400

    New 08-12 nightly for supersonic. Via @OMFBOT

--- supersonic.js
+++ supersonic.js
@@ -2,6 +2,16 @@
 {
 "device"="supersonic",
 "base"="1.3.1",
+"version"=08-12,
+"date"="08-12",
+"name"="OMFGB_supersonic-1.3.1_nightly-08-12.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/supersonic/OMFGB_supersonic-1.3.1_nightly-08-12.zip",
+"installable"="false",
+"description"="Fixed broken usb mounting from last night. More God Mode updates to clean and organize settings. Totally brand new "Bulletin Board" concept in God Mode."
+},
+{
+"device"="supersonic",
+"base"="1.3.1",
 "version"=08-11,
 "date"="08-11",
 "name"="OMFGB_supersonic-1.3.1_nightly-08-11.zip",
