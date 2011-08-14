73a1553cce351f9b98f5c0f7e9ad0cea6ff8a19d
r2DoesInc <r2doesinc@gmail.com>
2011-08-13 23:18:42 -0300

    alert

--- alerts.js
+++ alerts.js
@@ -1,12 +1,6 @@
 [
 {
 "device"="all",
-"name"="Hello Everyone!",
-"category"="r2doesinc",
-"description"="Welcome to the rom. This part is in development. REALLY cool things coming though."
-},
-{
-"device"="all",
 "name"="Whats is this?",
 "category"="r2doesinc",
 "description"="This is a section for us to post notes, updated in real time, to the users. I have plans for user replies to be implemented at a later time."
@@ -16,5 +10,11 @@
 "name"="God Mode FC",
 "category"="r2doesinc",
 "description"="The God Mode FC in the Nightlies section is now fixed. Please clear the DL cache and refresh your menifests."
+},
+{
+"device"="all",
+"name"="Delayed Builds",
+"category"="r2doesinc",
+"description"="No builds recently, as I think Im coming down with something. That and we havent done anything. Lemme kick this bug before we get back to work."
 }
 ]
