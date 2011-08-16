7ea6e4a30b0e8ac8b895c046cd0caeb37ed16eef
r2DoesInc <r2doesinc@gmail.com>
2011-08-16 19:41:13 -0300

    update

--- alerts.js
+++ alerts.js
@@ -5,16 +5,4 @@
 "category"="r2doesinc",
 "description"="This is a section for us to post notes, updated in real time, to the users. I have plans for user replies to be implemented at a later time."
 },
-{
-"device"="all",
-"name"="God Mode FC",
-"category"="r2doesinc",
-"description"="The God Mode FC in the Nightlies section is now fixed. Please clear the DL cache and refresh your menifests."
-},
-{
-"device"="all",
-"name"="Delayed Builds",
-"category"="r2doesinc",
-"description"="No builds recently, as I think Im coming down with something. That and we havent done anything. Lemme kick this bug before we get back to work."
-}
 ]
