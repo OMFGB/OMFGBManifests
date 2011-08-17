f11a49694a09c81f12c78ff124efb81c76cec342
r2DoesInc <r2doesinc@gmail.com>
2011-08-16 21:45:14 -0300

    test

--- addons.js
+++ addons.js
@@ -2,7 +2,7 @@
 //Google Apps
 {
 "device"="all",
-"name"="Google Appss",
+"name"="Google Apps",
 "zipname"="GAPPS.zip",
 "url"="http://r2doesinc.bitsurge.net/Addons/gapps-07-27-xx.zip",
 "installable"="false",
