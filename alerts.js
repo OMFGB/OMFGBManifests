bef43e6bbf35c1d2eb4e45d1383e2afeacdce6c4
r2DoesInc <r2doesinc@gmail.com>
2011-08-17 04:21:43 -0300

    Edited alerts.js via GitHub

--- alerts.js
+++ alerts.js
@@ -7,6 +7,12 @@
 "description"="This is a section for us to post notes, updated in real time, to the users. I have plans for user replies to be implemented at a later time."
 },
 {
+"device"="vivow",
+"name"="Help us get an Inc2",
+"category"="r2doesinc",
+"description"="Help us get an Incredible 2. Make it easier on us to bring you the best rom possible. Donate if you can, or if you have an extra Inc2, send it it!"
+},
+{
 "device"="all",
 "name"="Issues? Wipe and try again.",
 "category"="r2doesinc",
