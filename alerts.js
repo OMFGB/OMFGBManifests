8d6b3833fd7400444505ddf1e8a4f0cb45f12d11
r2DoesInc <r2doesinc@gmail.com>
2011-08-16 21:43:53 -0300

    test

--- alerts.js
+++ alerts.js
@@ -1,7 +1,7 @@
 [
 {
 "device"="all",
-"name"="Whats is this.....",
+"name"="Whats is this?",
 "category"="r2doesinc",
 "description"="This is a section for us to post notes, updated in real time, to the users. I have plans for user replies to be implemented at a later time."
 }
