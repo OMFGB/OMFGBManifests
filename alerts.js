0ffea918f14c273d2ffe39fe1987a2da98d550b8
r2DoesInc <r2doesinc@gmail.com>
2011-08-21 09:26:28 -0300

    remove old alerts

--- alerts.js
+++ alerts.js
@@ -7,22 +7,10 @@
 "description"="This is a section for us to post notes, updated in real time, to the users. I have plans for user replies to be implemented at a later time."
 },
 {
-"device"="mecha",
-"name"="8-17 Nightly",
-"category"="r2doesinc",
-"description"="The 8-17 build is nothing new, but somehow it never made it into the manifests. If your on an older build, I suggest you update."
-},
-{
 "device"="vivow",
 "name"="Help us get an Inc2",
 "category"="r2doesinc",
 "description"="Help us get an Incredible 2. Make it easier on us to bring you the best rom possible. Donate if you can, or if you have an extra Inc2, send it it!"
-},
-{
-"device"="all",
-"name"="Issues? Wipe and try again.",
-"category"="r2doesinc",
-"description"="Having issues with god mode and the new auto update setting? Wipe data and you should be good to go."
 },
 
 //xoomdev
