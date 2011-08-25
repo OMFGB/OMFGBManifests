4b9e349e1274ca8ffc6615e9590474f6add3300d
r2doesinc <r2doesinc@r2doesinc.columbus.rr.com>
2011-08-25 12:28:47 -0400

    New 08-25 nightly for vivow. Via @OMFBOT

--- vivow.js
+++ vivow.js
@@ -2,6 +2,16 @@
 {
 "device"="vivow",
 "base"="1.3.1",
+"version"=08-25,
+"date"="08-25",
+"name"="OMFGB_vivow-1.3.1_nightly-08-25.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/vivow/OMFGB_vivow-1.3.1_nightly-08-25.zip",
+"installable"="false",
+"description"="Fucking tons and tons. Touchwiz style notification shade music controls, many new tweaks from Roman --including the center clock mod--. Add Vibrant to list of supported devices. Fix Samsung lights sensor --I think--. Added touch to focus in the Camera for supported devices."
+},
+{
+"device"="vivow",
+"base"="1.3.1",
 "version"=08-23,
 "date"="08-23",
 "name"="OMFGB_vivow-1.3.1_nightly-08-23.zip",
