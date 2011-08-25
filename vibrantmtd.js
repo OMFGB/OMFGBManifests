91ce3d28931f99bca2870af647c4299b7c2a5964
r2doesinc <r2doesinc@r2doesinc.columbus.rr.com>
2011-08-25 17:43:14 -0400

    New 08-25 nightly for vibrantmtd. Via @OMFBOT

--- vibrantmtd.js
+++ vibrantmtd.js
@@ -1,2 +1,12 @@
 [
+{
+"device"="vibrantmtd",
+"base"="1.3.1",
+"version"=08-25,
+"date"="08-25",
+"name"="OMFGB_vibrantmtd-1.3.1_nightly-08-25.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/vibrantmtd/OMFGB_vibrantmtd-1.3.1_nightly-08-25.zip",
+"installable"="false",
+"description"="Fucking tons and tons. Touchwiz style notification shade music controls, many new tweaks from Roman --including the center clock mod--. Add Vibrant to list of supported devices. Fix Samsung lights sensor --I think--. Added touch to focus in the Camera for supported devices."
+},
 ]
