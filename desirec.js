56950cdd660e8319f5f5bc494c0596e138619857
r2doesinc <r2doesinc@r2doesinc.columbus.rr.com>
2011-08-25 14:50:51 -0400

    New 08-25 nightly for desirec. Via @OMFBOT

--- desirec.js
+++ desirec.js
@@ -2,6 +2,16 @@
 {
 "device"="desirec",
 "base"="1.3.1",
+"version"=08-25,
+"date"="08-25",
+"name"="OMFGB_desirec-1.3.1_nightly-08-25.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/desirec/OMFGB_desirec-1.3.1_nightly-08-25.zip",
+"installable"="false",
+"description"="Fucking tons and tons. Touchwiz style notification shade music controls, many new tweaks from Roman --including the center clock mod--. Add Vibrant to list of supported devices. Fix Samsung lights sensor --I think--. Added touch to focus in the Camera for supported devices."
+},
+{
+"device"="desirec",
+"base"="1.3.1",
 "version"=08-23,
 "date"="08-23",
 "name"="OMFGB_desirec-1.3.1_nightly-08-23.zip",
