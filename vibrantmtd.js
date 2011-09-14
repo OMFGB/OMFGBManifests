91a0de0d3146922a4a48c6b80ee5a38faa309724
r2doesinc <r2doesinc@r2doesinc.columbus.rr.com>
2011-09-14 04:50:51 -0400

    New 09-14 nightly for vibrantmtd. Via @OMFBOT

--- vibrantmtd.js
+++ vibrantmtd.js
@@ -2,6 +2,16 @@
 {
 "device"="vibrantmtd",
 "base"="1.3.1",
+"version"=09-14,
+"date"="09-14",
+"name"="OMFGB_vibrantmtd-1.3.1_nightly-09-14.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/vibrantmtd/OMFGB_vibrantmtd-1.3.1_nightly-09-14.zip",
+"installable"="false",
+"description"="So much I dont even know what, but it also includes the Sense 3.0 and new Honeycomb Lockscreen"
+},
+{
+"device"="vibrantmtd",
+"base"="1.3.1",
 "version"=08-25,
 "date"="08-25",
 "name"="OMFGB_vibrantmtd-1.3.1_nightly-08-25.zip",
