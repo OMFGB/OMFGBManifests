65f7597a0e94f3a891d86fee22f2a5e71fcddf1a
r2doesinc <r2doesinc@r2doesinc.columbus.rr.com>
2011-09-14 00:24:30 -0400

    New 09-14 nightly for supersonic. Via @OMFBOT

--- supersonic.js
+++ supersonic.js
@@ -2,6 +2,16 @@
 {
 "device"="supersonic",
 "base"="1.3.1",
+"version"=09-14,
+"date"="09-14",
+"name"="OMFGB_supersonic-1.3.1_nightly-09-13.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/supersonic/OMFGB_supersonic-1.3.1_nightly-09-13.zip",
+"installable"="false",
+"description"="So much I dont even know what, but it also includes the Sense 3.0 and new Honeycomb Lockscreen"
+},
+{
+"device"="supersonic",
+"base"="1.3.1",
 "version"=08-25,
 "date"="08-25",
 "name"="OMFGB_supersonic-1.3.1_nightly-08-25.zip",
