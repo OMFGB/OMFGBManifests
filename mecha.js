8c2dc4fe17cb8c2debd267e27e79005be206222e
xoomdev <xoomdevnet@gmail.com>
2011-09-30 15:34:01 -0500

    poo

--- mecha.js
+++ mecha.js
@@ -2,6 +2,16 @@
 {
 "device"="mecha",
 "base"="1.3.1",
+"version"=09-30x,
+"date"="09-30",
+"name"="OMFGB_mecha-1.3.1_nightly-09-30x.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/mecha/OMFGB_mecha-1.3.1_nightly-09-30x.zip",
+"installable"="false",
+"description"="Blame the bot damnit. Fix audio"
+},
+{
+"device"="mecha",
+"base"="1.3.1",
 "version"=09-30,
 "date"="09-30",
 "name"="OMFGB_mecha-1.3.1_nightly-09-30.zip",
