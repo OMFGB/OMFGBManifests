9b90f3d9541c4b5afeabf4ccdc6774afaedb6b45
r2doesinc <r2doesinc@r2doesinc.columbus.rr.com>
2011-10-06 03:54:47 -0400

    New 10-06 nightly for fascinatemtd. Via @OMFBOT

--- fascinatemtd.js
+++ fascinatemtd.js
@@ -2,6 +2,16 @@
 {
 "device"="fascinatemtd",
 "base"="1.3.1",
+"version"=10-06,
+"date"="10-06",
+"name"="OMFGB_fascinatemtd-1.3.1_nightly-10-06.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/fascinatemtd/OMFGB_fascinatemtd-1.3.1_nightly-10-06.zip",
+"installable"="false",
+"description"="2.3.7 update. God Mode flashing still broken. Droid X wifi fixed, gps still broken. Im tired AF."
+},
+{
+"device"="fascinatemtd",
+"base"="1.3.1",
 "version"=10-01,
 "date"="10-01",
 "name"="OMFGB_fascinatemtd-1.3.1_nightly-10-01.zip",
