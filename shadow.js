b1bc19cabc46e5e3241580ab1260686ff5bce583
r2doesinc <r2doesinc@r2doesinc.columbus.rr.com>
2011-10-06 05:44:14 -0400

    New 10-06 nightly for shadow. Via @OMFBOT

--- shadow.js
+++ shadow.js
@@ -2,6 +2,16 @@
 {
 "device"="shadow",
 "base"="1.3.1",
+"version"=10-06,
+"date"="10-06",
+"name"="OMFGB_shadow-1.3.1_nightly-10-06.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/shadow/OMFGB_shadow-1.3.1_nightly-10-06.zip",
+"installable"="false",
+"description"="2.3.7 update. God Mode flashing still broken. Droid X wifi fixed, gps still broken. Im tired AF."
+},
+{
+"device"="shadow",
+"base"="1.3.1",
 "version"=09-22,
 "date"="09-22",
 "name"="OMFGB_shadow-1.3.1_nightly-09-22.zip",
