a2a472ea3709eab10d87ad3037cb934e1a32267e
r2doesinc <r2doesinc@r2doesinc.columbus.rr.com>
2011-10-06 09:06:55 -0400

    New 10-06 nightly for inc. Via @OMFBOT

--- inc.js
+++ inc.js
@@ -2,6 +2,16 @@
 {
 "device"="inc",
 "base"="1.3.1",
+"version"=10-06,
+"date"="10-06",
+"name"="OMFGB_inc-1.3.1_nightly-10-06.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/inc/OMFGB_inc-1.3.1_nightly-10-06.zip",
+"installable"="false",
+"description"="2.3.7 update. God Mode flashing still broken. Droid X wifi fixed, gps still broken. Im tired AF."
+},
+{
+"device"="inc",
+"base"="1.3.1",
 "version"=09-22,
 "date"="09-22",
 "name"="OMFGB_inc-1.3.1_nightly-09-22.zip",
