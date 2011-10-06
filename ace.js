6398a50a8afd0c6b998dbc50b7a58d47234d3943
r2doesinc <r2doesinc@r2doesinc.columbus.rr.com>
2011-10-06 07:26:53 -0400

    New 10-06 nightly for ace. Via @OMFBOT

--- ace.js
+++ ace.js
@@ -1,3 +1,13 @@
 [
+{
+"device"="ace",
+"base"="1.3.1",
+"version"=10-06,
+"date"="10-06",
+"name"="OMFGB_ace-1.3.1_nightly-10-06.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/ace/OMFGB_ace-1.3.1_nightly-10-06.zip",
+"installable"="false",
+"description"="2.3.7 update. God Mode flashing still broken. Droid X wifi fixed, gps still broken. Im tired AF."
+},
 
 ]
