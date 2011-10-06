aa19bbf5f3e365741539de9ea8fe16bac8bf8144
r2doesinc <r2doesinc@r2doesinc.columbus.rr.com>
2011-10-06 15:46:31 -0400

    New 10-06 nightly for mesmerizemtd. Via @OMFBOT

--- mesmerizemtd.js
+++ mesmerizemtd.js
@@ -2,6 +2,16 @@
 {
 "device"="mesmerizemtd",
 "base"="1.3.1",
+"version"=10-06,
+"date"="10-06",
+"name"="OMFGB_mesmerizemtd-1.3.1_nightly-10-06.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/mesmerizemtd/OMFGB_mesmerizemtd-1.3.1_nightly-10-06.zip",
+"installable"="false",
+"description"="2.3.7 update. God Mode flashing still broken. Droid X wifi fixed, gps still broken. Im tired AF."
+},
+{
+"device"="mesmerizemtd",
+"base"="1.3.1",
 "version"=09-22,
 "date"="09-22",
 "name"="OMFGB_mesmerizemtd-1.3.1_nightly-09-22.zip",
