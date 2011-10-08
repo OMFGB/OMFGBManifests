e9c09c6a07ea932401d29b54f14324f174797945
r2doesinc <r2doesinc@r2doesinc.columbus.rr.com>
2011-10-08 06:49:01 -0400

    New 10-08 nightly for shadow. Via @OMFBOT

--- shadow.js
+++ shadow.js
@@ -2,6 +2,16 @@
 {
 "device"="shadow",
 "base"="1.3.1",
+"version"=10-08,
+"date"="10-08",
+"name"="OMFGB_shadow-1.3.1_nightly-10-08.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/shadow/OMFGB_shadow-1.3.1_nightly-10-08.zip",
+"installable"="false",
+"description"="Samsung Torch fixed. God Mode flashing broken and unfixable due to CWM5 changes. GM market links working. Fixes to LS and SB music controls. Fixes to MIUI battery bar. Status ba swipe brightness controls added."
+},
+{
+"device"="shadow",
+"base"="1.3.1",
 "version"=10-06,
 "date"="10-06",
 "name"="OMFGB_shadow-1.3.1_nightly-10-06.zip",
