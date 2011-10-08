97aac6d52bcf94137d9f86b222e772e88feec6d3
r2doesinc <r2doesinc@r2doesinc.columbus.rr.com>
2011-10-08 08:31:24 -0400

    New 10-08 nightly for ace. Via @OMFBOT

--- ace.js
+++ ace.js
@@ -2,6 +2,16 @@
 {
 "device"="ace",
 "base"="1.3.1",
+"version"=10-08,
+"date"="10-08",
+"name"="OMFGB_ace-1.3.1_nightly-10-08.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/ace/OMFGB_ace-1.3.1_nightly-10-08.zip",
+"installable"="false",
+"description"="Samsung Torch fixed. God Mode flashing broken and unfixable due to CWM5 changes. GM market links working. Fixes to LS and SB music controls. Fixes to MIUI battery bar. Status ba swipe brightness controls added."
+},
+{
+"device"="ace",
+"base"="1.3.1",
 "version"=10-06,
 "date"="10-06",
 "name"="OMFGB_ace-1.3.1_nightly-10-06.zip",
