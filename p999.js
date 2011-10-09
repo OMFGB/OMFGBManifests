b3e71dbbe54595b07f04ef10bd76816d90855f69
Ken Kyger <r2doesinc@Dev-1055T.(none)>
2011-10-08 21:34:14 -0400

    New 10-08 nightly for p999. Via @OMFBOT

--- p999.js
+++ p999.js
@@ -1,2 +1,12 @@
 [
+{
+"device"="p999",
+"base"="1.3.1",
+"version"=10-08,
+"date"="10-08",
+"name"="OMFGB_p999-1.3.1_nightly-10-08.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/p999/OMFGB_p999-1.3.1_nightly-10-08.zip",
+"installable"="false",
+"description"="Test build on Protekks server. God Mode flashing broken and unfixable due to CWM5 changes. GM market links working. Fixes to LS and SB music controls. Fixes to MIUI battery bar. Status ba swipe brightness controls added."
+},
 ]
