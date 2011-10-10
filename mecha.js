e7e2a96ddf0680bd7f7489cab7c7b5117092fdb4
Ken Kyger <r2doesinc@Dev-1055T.(none)>
2011-10-10 09:55:17 -0400

    New 10-10 nightly for mecha. Via @OMFBOT

--- mecha.js
+++ mecha.js
@@ -2,6 +2,16 @@
 {
 "device"="mecha",
 "base"="1.3.1",
+"version"=10-10,
+"date"="10-10",
+"name"="OMFGB_mecha-1.3.1_nightly-10-10.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/mecha/OMFGB_mecha-1.3.1_nightly-10-10.zip",
+"installable"="false",
+"description"="First build on Protekks server. Full on bug swatting mode. Closing in on v1.3.2"
+},
+{
+"device"="mecha",
+"base"="1.3.1",
 "version"=10-08,
 "date"="10-08",
 "name"="OMFGB_mecha-1.3.1_nightly-10-08.zip",
