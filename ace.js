df96b6c4115320d39758e0888861c47e49ab960b
Ken Kyger <r2doesinc@Dev-1055T.(none)>
2011-10-10 10:17:03 -0400

    New 10-10 nightly for ace. Via @OMFBOT

--- ace.js
+++ ace.js
@@ -2,6 +2,16 @@
 {
 "device"="ace",
 "base"="1.3.1",
+"version"=10-10,
+"date"="10-10",
+"name"="OMFGB_ace-1.3.1_nightly-10-10.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/ace/OMFGB_ace-1.3.1_nightly-10-10.zip",
+"installable"="false",
+"description"="First build on Protekks server. Full on bug swatting mode. Closing in on v1.3.2"
+},
+{
+"device"="ace",
+"base"="1.3.1",
 "version"=10-08,
 "date"="10-08",
 "name"="OMFGB_ace-1.3.1_nightly-10-08.zip",
