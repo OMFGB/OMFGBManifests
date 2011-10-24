85e02f956fb97a819b54aef8dff657467d57fe4b
r2doesinc <r2doesinc@gmail.com>
2011-10-24 04:00:15 -0400

    New OMGB build for ace. Via @OMGBOT

--- omgb/ace.js
+++ omgb/ace.js
@@ -1,3 +1,13 @@
 [
+{
+"device"="ace",
+"base"="1.2.3",
+"version"=10-24,
+"date"="10-24",
+"name"="OMGB_ace-1.2.3.zip",
+"url"="http://r2doesinc.bitsurge.net/omgb/ace/OMGB_ace-1.2.3.zip",
+"installable"="false",
+"description"="Updated to Android 2.3.7 base. Fixed data arrows on Inc2. Updated to GB kernel for Droid X (broken gps)."
+},
 
 ]
