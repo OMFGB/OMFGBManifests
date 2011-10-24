df944dd550455159978ec79f687fd5f486dfaa81
r2doesinc <r2doesinc@gmail.com>
2011-10-24 04:21:49 -0400

    New OMGB build for vivow. Via @OMGBOT

--- omgb/vivow.js
+++ omgb/vivow.js
@@ -1,6 +1,16 @@
 [
 {
 "device"="vivow",
+"base"="1.2.3",
+"version"=10-24,
+"date"="10-24",
+"name"="OMGB_vivow-1.2.3.zip",
+"url"="http://r2doesinc.bitsurge.net/omgb/vivow/OMGB_vivow-1.2.3.zip",
+"installable"="false",
+"description"="Updated to Android 2.3.7 base. Fixed data arrows on Inc2. Updated to GB kernel for Droid X (broken gps)."
+},
+{
+"device"="vivow",
 "version"="1.2.2",
 "date"="08-06",
 "name"="OMGB_vivow-1.2.2.zip",
