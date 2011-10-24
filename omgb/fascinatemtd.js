58c9e19e0404e5eda80268ec2105260ae9be0bd5
r2doesinc <r2doesinc@gmail.com>
2011-10-24 03:38:24 -0400

    New OMGB build for fascinatemtd. Via @OMGBOT

--- omgb/fascinatemtd.js
+++ omgb/fascinatemtd.js
@@ -1,6 +1,16 @@
 [
 {
 "device"="fascinatemtd",
+"base"="1.2.3",
+"version"=10-24,
+"date"="10-24",
+"name"="OMGB_fascinatemtd-1.2.3.zip",
+"url"="http://r2doesinc.bitsurge.net/omgb/fascinatemtd/OMGB_fascinatemtd-1.2.3.zip",
+"installable"="false",
+"description"="Updated to Android 2.3.7 base. Fixed data arrows on Inc2. Updated to GB kernel for Droid X (broken gps)."
+},
+{
+"device"="fascinatemtd",
 "version"="1.2.2",
 "date"="08-06",
 "name"="OMGB_fascinatemtd-1.2.2.zip",
