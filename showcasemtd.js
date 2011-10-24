e99e9e5cc1099af405c3bc4c66c07a4ae87abb8e
r2doesinc <r2doesinc@gmail.com>
2011-10-24 18:05:39 -0400

    New 10-24 nightly for showcasemtd. Via @OMFBOT

--- showcasemtd.js
+++ showcasemtd.js
@@ -2,6 +2,16 @@
 {
 "device"="showcasemtd",
 "base"="1.3.1",
+"version"=10-24,
+"date"="10-24",
+"name"="OMFGB_showcasemtd-1.3.1_nightly-10-24.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/showcasemtd/OMFGB_showcasemtd-1.3.1_nightly-10-24.zip",
+"installable"="false",
+"description"="We did some stuff, google finally posted sources again. fixed some samsung device bootlopping (I think). Got some Inc2 data arrows working."
+},
+{
+"device"="showcasemtd",
+"base"="1.3.1",
 "version"=10-12,
 "date"="10-12",
 "name"="OMFGB_showcasemtd-1.3.1_nightly-10-12.zip",
