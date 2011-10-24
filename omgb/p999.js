84c8d00b5e312236cc76b7ab8615c8270abaea8e
r2doesinc <r2doesinc@gmail.com>
2011-10-24 05:14:54 -0400

    New OMGB build for p999. Via @OMGBOT

--- omgb/p999.js
+++ omgb/p999.js
@@ -1,3 +1,13 @@
 [
+{
+"device"="p999",
+"base"="1.2.3",
+"version"=10-24,
+"date"="10-24",
+"name"="OMGB_p999-1.2.3.zip",
+"url"="http://r2doesinc.bitsurge.net/omgb/p999/OMGB_p999-1.2.3.zip",
+"installable"="false",
+"description"="Updated to Android 2.3.7 base. Fixed data arrows on Inc2. Updated to GB kernel for Droid X (broken gps)."
+},
 
 ]
