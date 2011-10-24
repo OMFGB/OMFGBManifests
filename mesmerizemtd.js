4c744b8358ca80cf351fc09001b363bf46d0f207
r2doesinc <r2doesinc@gmail.com>
2011-10-24 17:54:40 -0400

    New 10-24 nightly for mesmerizemtd. Via @OMFBOT

--- mesmerizemtd.js
+++ mesmerizemtd.js
@@ -2,6 +2,16 @@
 {
 "device"="mesmerizemtd",
 "base"="1.3.1",
+"version"=10-24,
+"date"="10-24",
+"name"="OMFGB_mesmerizemtd-1.3.1_nightly-10-24.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/mesmerizemtd/OMFGB_mesmerizemtd-1.3.1_nightly-10-24.zip",
+"installable"="false",
+"description"="We did some stuff, google finally posted sources again. fixed some samsung device bootlopping (I think). Got some Inc2 data arrows working."
+},
+{
+"device"="mesmerizemtd",
+"base"="1.3.1",
 "version"=10-12,
 "date"="10-12",
 "name"="OMFGB_mesmerizemtd-1.3.1_nightly-10-12.zip",
