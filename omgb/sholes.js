214cfaa13b32727e2f49fef7954347d4156262de
r2doesinc <r2doesinc@gmail.com>
2011-10-24 04:42:50 -0400

    New OMGB build for sholes. Via @OMGBOT

--- omgb/sholes.js
+++ omgb/sholes.js
@@ -1,6 +1,16 @@
 [
 {
 "device"="sholes",
+"base"="1.2.3",
+"version"=10-24,
+"date"="10-24",
+"name"="OMGB_sholes-1.2.3.zip",
+"url"="http://r2doesinc.bitsurge.net/omgb/sholes/OMGB_sholes-1.2.3.zip",
+"installable"="false",
+"description"="Updated to Android 2.3.7 base. Fixed data arrows on Inc2. Updated to GB kernel for Droid X (broken gps)."
+},
+{
+"device"="sholes",
 "version"="1.2.2",
 "date"="08-06",
 "name"="OMGB_sholes-1.2.2.zip",
