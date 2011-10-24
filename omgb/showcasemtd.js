d4d00dc2663a6cbcda3b81a5edea8d27211b0730
r2doesinc <r2doesinc@gmail.com>
2011-10-24 05:04:06 -0400

    New OMGB build for showcasemtd. Via @OMGBOT

--- omgb/showcasemtd.js
+++ omgb/showcasemtd.js
@@ -1,6 +1,16 @@
 [
 {
 "device"="showcasemtd",
+"base"="1.2.3",
+"version"=10-24,
+"date"="10-24",
+"name"="OMGB_showcasemtd-1.2.3.zip",
+"url"="http://r2doesinc.bitsurge.net/omgb/showcasemtd/OMGB_showcasemtd-1.2.3.zip",
+"installable"="false",
+"description"="Updated to Android 2.3.7 base. Fixed data arrows on Inc2. Updated to GB kernel for Droid X (broken gps)."
+},
+{
+"device"="showcasemtd",
 "version"="1.2.2",
 "date"="08-06",
 "name"="OMGB_showcasemtd-1.2.2.zip",
