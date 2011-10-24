da662ec4efef54c2e5beff9e91860d170f34e5bc
r2doesinc <r2doesinc@gmail.com>
2011-10-24 04:53:29 -0400

    New OMGB build for mesmerizemtd. Via @OMGBOT

--- omgb/mesmerizemtd.js
+++ omgb/mesmerizemtd.js
@@ -1,6 +1,16 @@
 [
 {
 "device"="mesmerizemtd",
+"base"="1.2.3",
+"version"=10-24,
+"date"="10-24",
+"name"="OMGB_mesmerizemtd-1.2.3.zip",
+"url"="http://r2doesinc.bitsurge.net/omgb/mesmerizemtd/OMGB_mesmerizemtd-1.2.3.zip",
+"installable"="false",
+"description"="Updated to Android 2.3.7 base. Fixed data arrows on Inc2. Updated to GB kernel for Droid X (broken gps)."
+},
+{
+"device"="mesmerizemtd",
 "version"="1.2.2",
 "date"="08-06",
 "name"="OMGB_mesmerizemtd-1.2.2.zip",
