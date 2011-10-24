c093ffa6486a10b6af8df9ce49767ac10e0af27b
r2doesinc <r2doesinc@gmail.com>
2011-10-24 17:32:38 -0400

    New 10-24 nightly for supersonic. Via @OMFBOT

--- supersonic.js
+++ supersonic.js
@@ -2,6 +2,16 @@
 {
 "device"="supersonic",
 "base"="1.3.1",
+"version"=10-24,
+"date"="10-24",
+"name"="OMFGB_supersonic-1.3.1_nightly-10-24.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/supersonic/OMFGB_supersonic-1.3.1_nightly-10-24.zip",
+"installable"="false",
+"description"="We did some stuff, google finally posted sources again. fixed some samsung device bootlopping (I think). Got some Inc2 data arrows working."
+},
+{
+"device"="supersonic",
+"base"="1.3.1",
 "version"=10-12,
 "date"="10-12",
 "name"="OMFGB_supersonic-1.3.1_nightly-10-12.zip",
